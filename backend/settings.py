f9b99c4886c2263754ffd5965d6970da43d4610a
Pokhvalenko <vsevolod.pohvalenko@gmail.com>
2020-11-01 13:40:42 +0200

    new database

--- backend/settings.py
+++ backend/settings.py
@@ -105,14 +105,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('BASE_NAME'),
+        'USER': config('BASE_USER'),
+        'PASSWORD': config('BASE_PASSWORD'),
+        'HOST': config('HOST', default='3306'),
+        'PORT': config('PORT'),
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
